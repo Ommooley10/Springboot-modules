e66fc79bdcfa4ef8fa44fde8580b404d64c8dd4a
Ommooley10 <omvikas1001@gmail.com>
2026-09-21 17:12:41 +0530

    Transforming API res using GlobalResponseHandler

--- module2/src/main/java/com/codingshuttle/om/module2/advices/GlobalExceptionHandler.java
+++ module2/src/main/java/com/codingshuttle/om/module2/advices/GlobalExceptionHandler.java
@@ -20,22 +20,25 @@ public class GlobalExceptionHandler {
     //RETURNS CUSTOMS DEFINED "APIERROR" RESPONSE WHICH CONTAIN A JSON FORMATTED STATUS CODE AND MESSAGE
     //NOTE: This exception handler is for handling only "NoSuchElementException" only
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(NoSuchElementException e){
-        ApiError apiError = ApiError.builder().httpStatus(HttpStatus.NOT_FOUND).message(e.getMessage()  ).build();
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(NoSuchElementException e){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .message(e.getMessage()  )
+                .build();
+        return new ResponseEntity<>(new ApiResponse<>(apiError), HttpStatus.NOT_FOUND);
     }
 
 
 
     /*To handle exceptions other than "NoSuchElementException" we created this another exception handler which can
-    * handle all another exceptions (eg internal server error)*/
+    * handle all another exceptions  (eg internal server error)*/
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerError(Exception e){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception e){
         ApiError apiError = ApiError.builder()
                 .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(e.getMessage())
                 .build();
 
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ApiResponse<>(apiError), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
